306274e7825582a88934d5ba0551681ca61ffc18
Rakib Ahmed <RAhmed@hvac.mea.com>
2023-07-01 17:10:36 -0400

    Public Player Card

--- components/smasherscape/smasherscape.tsx
+++ components/smasherscape/smasherscape.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
-
 export default function Smasherscape(props) {
     return <>
-        <div id={props.id} className={`${props.className} titleRow flex row`}>
-            Smasher Scape
-        </div>
-        <div className="playerGrid">
-            <Image width={500} height={300} src={`/assets/smasherscape/SmasherScapePlayerCard.png`} alt={`Smasherscape Player Card`} />
-            <img width={500} height={300} src={`https://next-13-vite-comparison.vercel.app/piratechs.svg`} alt={`Smasherscape Player Card`} />
+        <div id={props.id} className={`${props.className} playerGrid`}>
+            <img width={500} height={300} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/SmasherScapePlayerCard.png?raw=true`} alt={`Smasherscape Player Card`} />
         </div>
     </>
 }
