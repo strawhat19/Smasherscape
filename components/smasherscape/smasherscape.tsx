e32a35adeb430cacfc814fe21f4db5b15b560705
Rakib Ahmed <RAhmed@hvac.mea.com>
2023-07-01 17:44:48 -0400

    Fonts and Pics and Colors

--- components/smasherscape/smasherscape.tsx
+++ components/smasherscape/smasherscape.tsx
@@ -1,6 +1,12 @@
 export default function Smasherscape(props) {
     return <>
+        <h1 className="runescape_chat">RuneScape Chat</h1>
+        <h1 className="runescape_chat_bold">RuneScape Chat Bold</h1>
         <div id={props.id} className={`${props.className} playerGrid`}>
+            <img width={500} height={300} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/SmasherScapePlayerCard.png?raw=true`} alt={`Smasherscape Player Card`} />
+            <img width={500} height={300} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/SmasherScapePlayerCard.png?raw=true`} alt={`Smasherscape Player Card`} />
+            <img width={500} height={300} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/SmasherScapePlayerCard.png?raw=true`} alt={`Smasherscape Player Card`} />
+            <img width={500} height={300} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/SmasherScapePlayerCard.png?raw=true`} alt={`Smasherscape Player Card`} />
             <img width={500} height={300} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/SmasherScapePlayerCard.png?raw=true`} alt={`Smasherscape Player Card`} />
         </div>
     </>
