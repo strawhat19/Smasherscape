7e536200087238bd9e47fd7a5295362c9879c1b3
Rakib Ahmed <RAhmed@hvac.mea.com>
2023-07-01 19:14:18 -0400

    Tophat Logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -60,9 +60,9 @@ const config: DocsThemeConfig = {
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link rel="icon" href="https://next-13-vite-comparison.vercel.app/piratechs.svg" type="image/x-icon"></link>
+  <link rel="icon" href="https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/OSRS_Top_Hat.png?raw=true" type="image/x-icon"></link>
   </>,
-  logo: <Logo title={`Smasherscape`} color={`hsl(var(--nextra-primary-hue)100% 50%/1)`} />,
+  logo: <img width={40} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/OSRS_Top_Hat.png?raw=true`} alt={`Tophat Logo`} />,
   search: {
     placeholder: `Search...`
   },
