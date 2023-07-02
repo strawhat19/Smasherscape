69771e9de2f9fa9ed24c3ae8a60d99f59c8ae598
rakib <rahmed@hvac.mea.com>
2023-07-02 00:32:57 -0400

    Clean Cache

--- components/iframe.tsx
+++
@@ -1,5 +0,0 @@
-export default function Iframe(props) {
-    return <>
-        <iframe allowFullScreen title={props.title} allow={props.allow} src={props.src} style={{width: `100%`, height: `1080px`, margin: `20px auto`}} />
-    </>
-}
