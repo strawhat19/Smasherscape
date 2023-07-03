e3f6f6271c62314c19d8451be3513f054391ec90
rakib <rahmed@hvac.mea.com>
2023-07-03 00:32:00 -0400

    Update Check

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(props) {
     return <footer style={props.style}>
     <div className="left">
-        <a className="hoverLink" href="/">Home  <i className="fas fa-undo"></i></a>
+        <a className="hoverLink" title={`Home`} href="/">Home  <i className="fas fa-undo"></i></a>
     </div>
         <div className="right"> Xuruko <i className="fas fa-copyright"></i>{new Date().getFullYear()}</div>
 </footer>
