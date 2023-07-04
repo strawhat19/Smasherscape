3ff4a3a988572e96c974780b7e07720fa2bd22e4
Rakib Ahmed <RAhmed@hvac.mea.com>
2023-07-04 16:39:34 -0400

    Changes to Leveling

--- components/smasherscape.tsx
+++ components/smasherscape.tsx
@@ -1,11 +1,13 @@
+import  moment from 'moment';
 import { db } from '../firebase';
-import { FormEvent, useContext } from 'react';
+import { FormEvent, useContext, useRef } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
 import { defaultContent, formatDate, capitalizeAllWords, createXML, StateContext, showAlert } from '../pages/_app';
 
 export default function Smasherscape(props) {
 
+    const commandsInput = useRef();
     const { players, setPlayers, filteredPlayers, setFilteredPlayers } = useContext<any>(StateContext);
 
     const formInput = (e: FormEvent) => {
@@ -25,27 +27,146 @@ export default function Smasherscape(props) {
         }
     }
 
+    const calcPlyrWins = (plyr) => {
+        return plyr.plays.filter(ply => ply.winner.toLowerCase() == plyr.name.toLowerCase()).length;
+    }
+   
+    const calcPlyrLosses = (plyr) => {
+        return plyr.plays.filter(ply => ply.loser.toLowerCase() == plyr.name.toLowerCase()).length;
+    }
+
+    const renderLevelPic = (levelName) => {
+        if (levelName == `Bronze Scimitar`) {
+            return `https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/OSRS_Top_Hat.png?raw=true`;
+        } else {
+            return `https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/OSRS_Top_Hat.png?raw=true`;
+        }
+    }
+
+    const calcPlayerLevel = (plyr) => {
+        let xp = plyr.experience.arenaXP;
+
+        if (xp >= 83 && xp < 174) {
+            plyr.level.num = 2;
+            plyr.experience.nextLevelAt = 174;
+            plyr.experience.remainingXP = (174 - xp);
+        } else if (xp >= 174 && xp < 276) {
+            plyr.level.num = 3;
+            plyr.experience.nextLevelAt = 276;
+            plyr.experience.remainingXP = (276 - xp);
+        } else if (xp >= 276 && xp < 388) {
+            plyr.level.num = 4;
+            plyr.experience.nextLevelAt = 388;
+            plyr.experience.remainingXP = (388 - xp);
+        } else if (xp >= 388 && xp < 512) {
+            plyr.level.num = 5;
+            plyr.experience.nextLevelAt = 512;
+            plyr.experience.remainingXP = (512 - xp);
+        } else if (xp >= 512 && xp < 650) {
+            plyr.level.num = 6;
+            plyr.experience.nextLevelAt = 650;
+            plyr.experience.remainingXP = (650 - xp);
+        } else if (xp >= 650 && xp < 801) {
+            plyr.level.num = 7;
+            plyr.experience.nextLevelAt = 801;
+            plyr.experience.remainingXP = (801 - xp);
+        } else if (xp >= 801 && xp < 969) {
+            plyr.level.num = 8;
+            plyr.experience.nextLevelAt = 969;
+            plyr.experience.remainingXP = (969 - xp);
+        } else if (xp >= 969 && xp < 1154) {
+            plyr.level.num = 9;
+            plyr.experience.nextLevelAt = 1154;
+            plyr.experience.remainingXP = (1154 - xp);
+        } else if (xp >= 1154 && xp < 1358) {
+            plyr.level.num = 10;
+            plyr.experience.nextLevelAt = 1358;
+            plyr.experience.remainingXP = (1358 - xp);
+        }
+
+        return plyr;
+    }
+
     const formSubmit = (e: FormEvent) => {
         e.preventDefault();
-        // let field = e.target as HTMLInputElement;
-        // if (field.name == `commands`) {
-            console.log(`Form Submit`, e.target);
-        // } else {
-            // return;
-        // }
+        let field = commandsInput.current as HTMLInputElement;
+        if (field.name == `commands`) {
+            let command = field?.value.toLowerCase();
+            let commandParams = command.split(` `);
+            
+            if (command != ``) {
+                if (commandParams[0] == `!upd`) {
+                    let playerOne = commandParams[1];
+                    let playerTwo = commandParams[3];
+                    let characterOne = commandParams[5];
+                    let characterTwo = commandParams[7];
+                    let stocksTaken = parseInt(commandParams[8]) || 0;
+                    let date = moment().format(`MMMM Do YYYY, h:mm:ss a`);
+
+                    let playerOneDB = players.find(plyr => plyr?.name.toLowerCase() == playerOne || plyr?.name.toLowerCase().includes(playerOne));
+                    let playerTwoDB = players.find(plyr => plyr?.name.toLowerCase() == playerTwo || plyr?.name.toLowerCase().includes(playerTwo));
+
+                    if (!playerOneDB || !playerTwoDB) {
+                        alert(`Can't find players with those names.`);
+                        return;
+                    } else if (!characterOne || !characterTwo) {
+                        alert(`Which characters did they play?`);
+                        return;
+                    } else {
+
+                        let updatedPlayers = players.map(plyr => {
+                            if (plyr?.name.toLowerCase() == playerOne || plyr?.name.toLowerCase().includes(playerOne)) {
+                                plyr.experience.arenaXP = plyr.experience.arenaXP + 400;
+                                plyr.plays.push({
+                                    winner: playerOneDB?.name,
+                                    loser: playerTwoDB?.name,
+                                    character: characterOne,
+                                    stocksTaken,
+                                    date
+                                });
+
+                                calcPlayerLevel(plyr);
+
+                                return plyr;
+                            } else if (plyr?.name.toLowerCase() == playerTwo || plyr?.name.toLowerCase().includes(playerTwo)) {
+                                plyr.experience.arenaXP = plyr.experience.arenaXP + (100 * stocksTaken);
+                                plyr.plays.push({
+                                    winner: playerOneDB?.name,
+                                    loser: playerTwoDB?.name,
+                                    character: characterTwo,
+                                    stocksTaken,
+                                    date
+                                });
+
+                                calcPlayerLevel(plyr);
+
+                                return plyr;
+                            } else {
+                                return plyr;
+                            }
+                        });
+
+                        console.log(`Updated Players`, updatedPlayers);
+                        setPlayers(updatedPlayers);
+                    }
+                }
+            }
+        } else {
+            return;
+        }
     }
 
     return <>
         <form onInput={(e) => formInput(e)} onSubmit={(e) => formSubmit(e)} action="submit" className="gridForm">
             <div className={`inputWrapper`}><div className="inputBG"></div><input type="search" className="search" name={`search`} placeholder={`Search...`} /></div>
-            <div className={`inputWrapper`}><div className="inputBG"></div><input type="text" className="commands" name={`commands`} placeholder={`Commands...`} /></div>
+            <div className={`inputWrapper`}><div className="inputBG"></div><input ref={commandsInput} type="text" className="commands" name={`commands`} placeholder={`Commands...`} /></div>
             <button style={{display: `none`}} type={`submit`}>Submit</button>
         </form>
         <div id={props.id} className={`${props.className} playerGrid ${filteredPlayers.length == 0 ? `empty` : `populated`}`}>
             {filteredPlayers.length == 0 && <>
                 <div className="gridCard"><h1 className={`runescape_large noPlayersFound`}>No Players Found</h1></div>
             </>}
-            {filteredPlayers.length > 0 && filteredPlayers.map((plyr, plyrIndex) => {
+            {filteredPlayers.length > 0 && filteredPlayers.sort((a,b) => b.experience.arenaXP - a.experience.arenaXP).map((plyr, plyrIndex) => {
                 return (
                     <div className="gridCard" key={plyrIndex}>
                         <LazyLoadImage effect="blur" src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/OSRS_Card_Empty.png?raw=true`} className={`cardBG`} alt={`Smasherscape Player Card`} />
@@ -60,17 +181,25 @@ export default function Smasherscape(props) {
                             </div>
                             <div className="cardMiddleRow">
                                 <div className="imgLeftCol">
-                                    <img width={150} src={`https://github.com/strawhat19/Smasherscape/blob/main/assets/smasherscape/OSRS_Top_Hat.png?raw=true`} alt={`Tophat Logo`} />
-                                    <h4 className={`levelName`}>Bronze Scimitar</h4>
+                                    <img width={150} src={renderLevelPic(plyr?.level?.name)} alt={plyr?.level?.name} />
+                                    <h4 className={`levelName`}>{plyr?.level?.name}</h4>
                                 </div>
                                 <div className="recordPlays">
                                     <div className="record">
                                         <h3>Record</h3>
-                                        <h4>0 - 0</h4>
+                                        <h4>{calcPlyrWins(plyr)} - {calcPlyrLosses(plyr)}</h4>
                                     </div>
                                     <div className="plays">
-                                        <h3>Exp</h3>
-                                        <h4>{plyr?.experience?.xp}</h4>
+                                        <h3>Plays</h3>
+                                        <div className={`playsContainer`}>
+                                            {plyr.plays.map(ply => ply.character).map((char, charIndex) => {
+                                                return (
+                                                    <div key={charIndex} className={`charImg`}>
+                                                        {char}
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="rightCol">
@@ -96,7 +225,7 @@ export default function Smasherscape(props) {
                                 </div>
                             </div>
                             <div className="cardBottomRow">
-                                <div className="gradient" style={{clipPath: `polygon(0% 0, ${plyr.experience.xp / plyr.experience.remainingXP}% 0%, ${plyr.experience.xp / plyr.experience.remainingXP}% 100%, 0 100%)`}}></div>
+                                <div className="gradient" style={{clipPath: `polygon(0% 0, ${(plyr.experience.arenaXP / plyr.experience.nextLevelAt) * 100}% 0%, ${(plyr.experience.arenaXP / plyr.experience.nextLevelAt) * 100}% 100%, 0 100%)`}}></div>
                             </div>
                         </div>
                     </div>
