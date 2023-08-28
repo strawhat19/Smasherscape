e221f365be8c8c3c3b910f5be05a058952490404
rakib <rahmed@hvac.mea.com>
2023-08-28 06:53:43 -0400

    Navbar Updates

---
+++ components/Form.tsx
@@ -0,0 +1,175 @@
+import { useContext, useEffect, useRef, useState } from 'react';
+import { capitalizeAllWords, StateContext, showAlert } from '../pages/_app';
+
+export default function Form(props?: any) {
+  const { style } = props;
+  const loadedRef = useRef(false);
+  const [loaded, setLoaded] = useState(false);
+  const [alertOpen, setAlertOpen] = useState(false); 
+  const { user, setUser, updates, setUpdates, authState, setAuthState, emailField, setEmailField, users, setFocus } = useContext<any>(StateContext);
+
+  const authForm = (e?: any) => {
+    e.preventDefault();
+    let formFields = e.target.children;
+    let clicked = e?.nativeEvent?.submitter;
+    let email = formFields?.email?.value ?? `email`;
+
+    switch(clicked?.value) {
+      default:
+        console.log(clicked?.value);
+        break;
+      case `Next`:
+        // getDocs(collection(db, `users`)).then((snapshot) => {
+        //   let latestUsers = snapshot.docs.map((doc: any) => doc.data()).sort((a: any, b: any) => b?.highScore - a?.highScore);
+        //   let macthingEmails = latestUsers.filter((usr: any) => usr?.email.toLowerCase() == email.toLowerCase());
+        //   setUsers(latestUsers);
+        // let arr = [];
+        // setEmailField(true);
+        // if (arr.length > 0) {
+        //   // localStorage.setItem(`account`, JSON.stringify(macthingEmails[0]));
+        //   setAuthState(`Sign In`);
+        // } else {
+        //   setAuthState(`Sign Up`);
+        // }
+        // });
+        showAlert(`Whoah There!`, <div className={`formWarning`}>
+          <h2 style={{fontSize: `1.5em`}}>
+            Ok so, I haven't quite put user authentication in yet, I will soon! For now, this is not supported yet, apologies!
+          </h2>
+        </div>, `420px`, `auto`);
+        break;
+      case `Back`:
+        setUpdates(updates+1);
+        setAuthState(`Next`);
+        setEmailField(false);
+        break;
+      case `Sign Out`:
+        setUser(null);
+        setAuthState(`Next`);
+        setEmailField(false);
+        setUpdates(updates+1);
+        // localStorage.removeItem(`user`);
+        // localStorage.removeItem(`users`);
+        // localStorage.removeItem(`lists`);
+        // localStorage.removeItem(`score`);
+        // localStorage.removeItem(`health`);
+        // localStorage.removeItem(`account`);
+        // localStorage.removeItem(`highScore`);
+        break;
+      case `Save`:
+        let emptyFields = [];
+        let fieldsToUpdate = [];
+
+        console.log(formFields);
+
+        for (let i = 0; i < formFields.length; i++) {
+          const input = formFields[i];
+          if (input?.classList?.contains(`userData`)) {
+            if (input.value === ``) {
+              emptyFields.push(input?.placeholder);
+            } else {
+              fieldsToUpdate.push(input);
+            }
+          }
+        }
+
+        if (fieldsToUpdate.length == 0) {
+          showAlert(`The Form was NOT Saved.`, `You Can Fill`, emptyFields);
+        } else {
+            console.log(`Fields To Update`, fieldsToUpdate);
+        //   let updatedUser = { ...user, updated: formatDate(new Date()) };
+        //   Object.assign(updatedUser, ...([...fieldsToUpdate].map(input => {
+        //     if (input?.classList?.contains(`userData`)) {
+        //       if (input?.id == `bio`) setContent(formFields?.bio?.value);
+        //       return {[input.id]: input.value}
+        //     }
+        //   })));
+          // addOrUpdateUser(user?.id, updatedUser);
+        }
+        break;
+      case `Sign In`:
+        let password = formFields?.password?.value;
+
+        if (password == ``) {
+          showAlert(`Password Required`);
+        } else { // Successful Sign In
+        //   if (password == existingAccount?.password) {
+            setFocus(false);
+            setAuthState(`Sign Out`);
+            // setUser(existingAccount);
+            // setColor((existingAccount?.color || `#000000`));
+            // getDocs(collection(db, `users`)).then((snapshot) => setUsers(snapshot.docs.map((doc: any) => doc.data()).sort((a: any, b: any) => b?.highScore - a?.highScore)));
+        //   } else {
+            // showAlert(`Invalid Password`);
+        //   }
+        }
+        break;
+      case `Sign Up`:
+        let name = capitalizeAllWords(email.split(`@`)[0]);
+        setAuthState(`Signed Up`);
+
+        // getDocs(collection(db, `users`)).then((snapshot) => {
+        //   let latestUsers = snapshot.docs.map((doc: any) => doc.data());
+        //   let macthingEmails = latestUsers.filter((usr: any) => usr?.email.toLowerCase() == email.toLowerCase());
+        //   setUsers(latestUsers);
+        //   if (macthingEmails.length > 0) {
+        //     localStorage.setItem(`account`, JSON.stringify(macthingEmails[0]));
+        //     setAuthState(`Sign In`);
+        //   } else {
+        //     setAuthState(`Sign Up`);
+        //   }
+
+        //   let password = formFields?.password?.value;
+        //   if (password == ``) {
+        //     showAlert(`Password Required`);
+        //   } else {
+        //     setFocus(false);
+        //     let storedHighScore = JSON.parse(localStorage.getItem(`highScore`) as any);
+        //     let potentialUser = { 
+        //       id: users.length + 1, 
+        //       bio: ``,
+        //       color: ``, 
+        //       number: 0,
+        //       status: ``,
+        //       name: name, 
+        //       email: email,
+        //       roles: [`user`],
+        //       password: password, 
+        //       lists: defaultLists,
+        //       updated: formatDate(new Date()), 
+        //       lastSignin: formatDate(new Date()), 
+        //       registered: formatDate(new Date()), 
+        //       highScore: Math.floor(storedHighScore) || 0,
+        //     };
+  
+        //     let uuid = genUUID(latestUsers, potentialUser);
+        //     // addOrUpdateUser(uuid, potentialUser);
+        //     getDocs(collection(db, `users`)).then((snapshot) => setUsers(snapshot.docs.map((doc: any) => doc.data()).sort((a: any, b: any) => b?.highScore - a?.highScore)));
+        //     setAuthState(`Sign Out`);
+        //   }
+        // });
+        break;
+    };
+  }
+
+  useEffect(() => {
+    if (loadedRef.current) return;
+    loadedRef.current = true;
+    setLoaded(true);
+  }, [user, users, authState]);
+
+  return <>
+  <form id={props.id} onSubmit={authForm} className={`flex authForm customButtons ${props.className}`} style={style}>
+      {!user && <input placeholder="Email" type="email" name="email" autoComplete={`email`} required />}
+      {!user && emailField && <input placeholder="Password (Use a password that you dont care about, but its still easy to remember)" type="password" name="password" autoComplete={`current-password`} />}
+      {user && window?.location?.href?.includes(`profile`) && <input id="name" className={`name userData`} placeholder="Name" type="text" name="status" />}
+      {user && window?.location?.href?.includes(`profile`) && <input id="status" className={`status userData`} placeholder="Status" type="text" name="status" />}
+      {user && window?.location?.href?.includes(`profile`) && <input id="bio" className={`bio userData`} placeholder="About You" type="text" name="bio" />}
+      {user && window?.location?.href?.includes(`profile`) && <input id="number" className={`number userData`} placeholder="Favorite Number" type="number" name="number" />}
+      {user && window?.location?.href?.includes(`profile`) && <input id="password" className={`editPassword userData`} placeholder="Edit Password" type="password" name="editPassword" autoComplete={`current-password`} />}
+      <input className={(user && window?.location?.href?.includes(`profile`) || (authState == `Sign In` || authState == `Sign Up`)) ? `submit half` : `submit full`} type="submit" name="authFormSubmit" value={user ? `Sign Out` : authState} />
+      {(authState == `Sign In` || authState == `Sign Up`) && <input id={`back`} className={`back`} type="submit" name="authFormBack" value={`Back`} />}
+      {user && window?.location?.href?.includes(`profile`) && <input id={user?.id} className={`save`} type="submit" name="authFormSave" style={{padding: 0}} value={`Save`} />}
+    </form>
+  </>
+}
---
+++ components/NextraSearch.tsx
@@ -0,0 +1,13 @@
+export const searchFromString = (e) => console.log(`Search Players`, e);
+export default function NextraSearch(props) {
+    return  (
+        <div className="nextra-search nx-relative md:nx-w-64 nx-hidden md:nx-inline-block mx-min-w-[200px]">
+            <div className="nx-relative nx-flex nx-items-center nx-text-gray-900 contrast-more:nx-text-gray-800 dark:nx-text-gray-300 contrast-more:dark:nx-text-gray-300">
+                {props.children ?? <>
+                    <input id={`globalSearchInput`} onInput={(e) => searchFromString(e)} className="nx-block nx-w-full nx-appearance-none nx-rounded-lg nx-px-3 nx-py-2 nx-transition-colors nx-text-base nx-leading-tight md:nx-text-sm nx-bg-black/[.05] dark:nx-bg-gray-50/10 focus:nx-bg-white dark:focus:nx-bg-dark placeholder:nx-text-gray-500 dark:placeholder:nx-text-gray-400 contrast-more:nx-border contrast-more:nx-border-current" type="search" placeholder="Search..." value="" />
+                    <kbd className="nx-absolute nx-my-1.5 nx-select-none ltr:nx-right-1.5 rtl:nx-left-1.5 nx-h-5 nx-rounded nx-bg-white nx-px-1.5 nx-font-mono nx-text-[10px] nx-font-medium nx-text-gray-500 nx-border dark:nx-border-gray-100/20 dark:nx-bg-dark/50 contrast-more:nx-border-current contrast-more:nx-text-current contrast-more:dark:nx-border-current nx-items-center nx-gap-1 nx-pointer-events-none nx-hidden sm:nx-flex nx-opacity-100">CTRL K</kbd>
+                </>}
+            </div>
+        </div>
+    )
+}
