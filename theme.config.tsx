d5485314f65e307ce5e91ea9be6a194d074c6114
rakib <rahmed@hvac.mea.com>
2023-08-28 06:56:01 -0400

    Navbar Updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import NextraSearch from './components/NextraSearch';
 
