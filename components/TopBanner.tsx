610fdf9a8dfa856d3a10d04301524fed82126fad
rakib <rahmed@hvac.mea.com>
2024-08-19 01:49:35 -0400

    Testing Icons and Top Nav

--- components/TopBanner.tsx
+++ components/TopBanner.tsx
@@ -1,10 +1,40 @@
+import { navFaIcons } from "./NavIcons";
+import { useEffect, useState } from "react";
+
 export default function TopBanner() {
+    let [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!loaded) {
+            let navLinks = document.querySelectorAll(`nav a.nx-text-sm`);
+            navLinks.forEach((navItem: any, index) => {
+                let linkText = navItem.querySelector(`.nx-absolute`);
+                if (linkText != null) {
+                    let icon = document.createElement(`i`);
+                    icon[`key`] = index;
+                    icon.classList.add(`fas`);
+                    icon.classList.add(`navIconColor`);
+                    icon.classList.add(navFaIcons[index].faIconName);
+                    linkText.prepend(icon);
+                }
+            })
+        }
+
+        setLoaded(true);
+    }, [])
+
     return <>
         <div className={`topBanner`}>
             <div className={`topNavigation`}>
-                <a href={`/404`} className={`hoverLink`}>
-                    Banner
+                Smasherscape
+                {/* <a href={`/arenas`} className={`hoverLink`}>
+                    <i className={`fas fa-trophy navIcon black`} />
+                    Arenas
                 </a>
+                <a href={`/plays`} className={`hoverLink`}>
+                    <i className={`fas fa-gamepad navIcon black`} />
+                    Plays
+                </a> */}
             </div>
         </div>
     </>
